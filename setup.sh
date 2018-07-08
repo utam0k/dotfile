ee8b601db991931edfbbc9ce5dfb713c9ef0db12
utam0k <k0ma@utam0k.jp>
2018-07-08 13:38:03 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 function ConfirmGui() {
-	echo "GUI? yes/no"
-	read input
-	if [ -z $input ] ; then
-		echo "yes または no を入力して下さい."
-		ConfirmGui
-	elif [ $input != 'no' ] || [ $input != 'NO' ] || [ $input != 'n' ] ; then
-		echo "Install withoud GUI"
-	elif [ $input = 'yes' ] || [ $input = 'YES' ] || [ $input = 'y' ] ; then
-		$basename/pkg/chrome
-	fi
+    echo "GUI? yes/no"
+    read input
+    if [ -z $input ] ; then
+        echo "yes または no を入力して下さい."
+        ConfirmGui
+    elif [ $input = 'no' ] || [ $input = 'NO' ] || [ $input = 'n' ] ; then
+        echo "Install without GUI"
+    elif [ $input = 'yes' ] || [ $input = 'YES' ] || [ $input = 'y' ] ; then
+        $basename/pkg/chrome.sh
+        find install -name "*.sh" | sh
+    fi
 }
 
 basename=`dirname $0`
