f3a9eeba2a55ae90a132134631e628514a352ea4
utam0k <k0ma@utam0k.jp>
2019-10-15 19:36:37 +0900

    update

--- zsh/zshrc.d/hooks/zplug.zsh
+++ zsh/zshrc.d/hooks/zplug.zsh
@@ -12,9 +12,11 @@ export ZPLUG_BIN=$ZPLUG_REPOS/bin
 
 source $ZPLUG_HOME/init.zsh
 
-zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-completions", lazy:true
-zplug "zsh-users/zsh-syntax-highlighting"
+plugins=("zsh-users/zsh-autosuggestions" "zsh-users/zsh-completions" "zsh-users/zsh-syntax-highlighting")
+
+for p in $plugins; do
+    zplug "$p"
+done
 
 
 zplug check --verbose >&2 || zplug install >&2
@@ -23,13 +25,10 @@ zplug update >&2
 zplug load --verbose >&2
 zplug clear >&2
 
-list=(`zplug list`)
-
 echo "##### zplug #####"
 
-for i in $list ; do
-    eval $(zplug info $i | sed -e 's/-//' | sed -e 's/: /=/'|grep "dir" | sed 's/\x1b\[[0-9;]*m//g')
-    echo source $(ls $dir/*.zsh | head -n1)
+for plugin in $plugins; do
+    echo source $(ls $PWD/../../build/zsh-plugins/$plugin/*.zsh | /usr/bin/head -n1)
 done
 
 echo "##### /zplug #####"
