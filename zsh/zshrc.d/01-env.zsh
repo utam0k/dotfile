ea0c3bfeedb0e23c95d6b0b15007b3cb1cfa064e
utam0k <k0ma@utam0k.jp>
2019-11-03 16:20:41 +0900

    set EDITOR.

--- zsh/zshrc.d/01-env.zsh
+++ zsh/zshrc.d/01-env.zsh
@@ -1,3 +1,10 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=100000000
 SAVEHIST=${HISTSIZE}
+if command -v nvim > /dev/null; then
+    EDITOR=$(which nvim)
+elif command -v vim > /dev/null; then
+    EDITOR=$(which vim)
+else
+    EIDTOR=$(which vi)
+fi
