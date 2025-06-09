e0f39fefac8bf77eb8b69ef2748526042f69e2a5
utam0k <k0ma@utam0k.jp>
2025-06-09 15:23:40 +0900

    nvim: flash enables `/`
    Signed-off-by: utam0k <k0ma@utam0k.jp>

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,7 +2,50 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    -- Enable flash for search
+    search = {
+      enabled = true,
+      highlight = {
+        backdrop = false,
+      },
+      jump = {
+        history = true,
+        register = true,
+        nohlsearch = true,
+      },
+      exclude = {
+        "cmp_menu",
+        "flash_prompt",
+        function(win)
+          -- Exclude non-focusable windows
+          return not vim.api.nvim_win_get_config(win).focusable
+        end,
+      },
+    },
+    -- Flash when searching with / ? * # g* g#
+    modes = {
+      search = {
+        enabled = true,
+        highlight = { backdrop = false },
+        jump = { history = true, register = true, nohlsearch = true },
+      },
+      char = {
+        enabled = true,
+        -- Hide after jump when not using jump labels
+        autohide = false,
+        jump_labels = true,
+        multi_line = false,
+      },
+    },
+    label = {
+      uppercase = false,
+      rainbow = {
+        enabled = true,
+        shade = 5,
+      },
+    },
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
