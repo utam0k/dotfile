e34c02a4a1b911f3a9136ad4bff8d2a3fb4fbc09
utam0k <k0ma@utam0k.jp>
2025-06-23 21:23:57 +0900

    refactor(nvim): clean up options.lua
    - Remove outdated comments about Neovim defaults
    - Add diff options for better code review experience
    - Improve comment formatting and consistency
    - Clean up file handling and appearance settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,14 +6,20 @@ opt.expandtab = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
-opt.smartindent = true  -- autoindent is enabled by default in Neovim
+opt.smartindent = true
 
 -- Display
 opt.wrap = false
 opt.wildmode = "list,full"
 
 -- Performance
-opt.updatetime = 300  -- Faster CursorHold for auto-reload (default is 4000)
+opt.updatetime = 300 -- Faster CursorHold for auto-reload
+
+-- Diff options for better code review (using only supported options)
+vim.cmd([[
+  set diffopt+=algorithm:patience
+  set diffopt+=indent-heuristic
+]])
 
 -- Persistent undo
 opt.undodir = vim.fn.expand("$HOME/.config/nvim/undo")
@@ -21,19 +27,26 @@ opt.undofile = true
 
 -- File handling
 opt.autoread = true -- Automatically read file when changed outside of Vim
-opt.backup = false  -- Don't create backup files
+opt.backup = false -- Don't create backup files
 opt.swapfile = false -- Don't create swap files
 
+-- Disable tags
+opt.tags = "" -- Prevents E433 error when no tags file exists
+
 -- Appearance
-opt.termguicolors = true  -- Enable 24-bit RGB colors
-opt.mouse = ""            -- Disable mouse support
-opt.cursorline = true     -- Highlight current line
-opt.background = "dark"   -- Tell plugins we prefer dark colorschemes
-opt.number = true         -- Show line numbers
--- Note: 'laststatus=2' is the default in Neovim (always show statusline)
--- Note: 'syntax on' is not needed when using treesitter for highlighting
--- Note: 'encoding=utf-8' is the default in Neovim
--- Note: 'lazyredraw' is deprecated in Neovim and has no effect
+opt.termguicolors = true -- Enable 24-bit RGB colors
+opt.mouse = "" -- Disable mouse support
+opt.cursorline = true -- Highlight current line
+opt.background = "dark" -- Tell plugins we prefer dark colorschemes
+opt.number = true -- Show line numbers
+opt.cmdheight = 0 -- Hide command line when not in use
+opt.showmode = false -- Don't show mode in command line
+opt.laststatus = 0 -- Hide statusline completely
+
+-- Suppress file messages
+opt.shortmess:append("F") -- Don't give file info when editing a file
+opt.shortmess:append("c") -- Don't give completion menu messages
+opt.report = 99999 -- Threshold for reporting number of changed lines (effectively disable)
 
 -- Python host programs for plugins that need Python support
 vim.g.python_host_prog = vim.fn.expand("$HOME/.pyenv/versions/nvim/bin/python")
